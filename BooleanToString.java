b1e3859f194270cdb3570c04351b03f8c7a960dc
D Davis <dfdx2@outlook.com>
2022-05-23 12:20:01 -0400

    Adding additional CodeWars solutions

---
+++ BooleanToString.java
@@ -0,0 +1,7 @@
+public class BooleanToString {
+  public static String convert(boolean b){
+    
+  String s1 = Boolean.toString(b);
+  return s1;
+  }
+}
---
+++ StringToNumber.java
@@ -0,0 +1,7 @@
+public class StringToNumber {
+  public static int stringToNumber(String str) {
+    //TODO: Convert str into a number
+    int i = Integer.parseInt(str);
+    return i;
+  }
+}
