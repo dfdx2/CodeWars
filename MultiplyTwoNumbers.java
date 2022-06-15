908d94b56eacac8cb7efdfc787a80f945fe53fff
D Davis <dfdx2@outlook.com>
2022-06-15 18:34:29 -0400

    Adding new solution

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,9 @@
+/*    Implement a fucktion which multiplies two numbers
+ *    Make a function with the class name Kata and the function name multiply 
+ *    that takes in two integers and multiplies them
+ */
+class Kata {
+  public static int multiply(int x, int y) {
+    return x * y;
+  }
+}
