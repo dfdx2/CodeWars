8e576bd630b9d0bcc3fec54ceff07a96096c453e
D Davis <dfdx2@outlook.com>
2022-06-25 08:53:36 -0400

    Adding new solution

---
+++ SquareSum.java
@@ -0,0 +1,18 @@
+/*
+Complete the square sum function so that it squares each number 
+passed into it and then sums the results together.
+For example, for [1, 2, 2] it should 
+return 9 because 1^2 + 2^2 + 2^2 = 9.
+*/
+
+
+public class Kata {
+  public static int squareSum(int[] n) { 
+     int power = 2;
+     int sum = 0;
+       for(int i = 0; i < n.length; i++) {
+         sum += Math.pow(n[i], power);
+       }
+    return sum;
+  }
+}
