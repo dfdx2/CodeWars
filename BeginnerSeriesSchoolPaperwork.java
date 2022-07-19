8af7894750070ee97c29b99a24f9f8375a3c7dac
D Davis <dfdx2@outlook.com>
2022-07-18 20:01:08 -0400

    Adding new solution

---
+++ BeginnerSeriesSchoolPaperwork.java
@@ -0,0 +1,59 @@
+/*  8KYU CodeWars Beginner Series #1 School Paperwork
+
+Your classmates asked you to copy some paperwork for them. 
+You know that there are 'n' classmates and the paperwork has 'm' pages.
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+Example:
+n= 5, m=5: 25
+n=-5, m=5:  0
+Waiting for translations and Feedback! Thanks!
+
+*/
+
+public class Paper
+{
+  public static int paperWork(int n, int m) 
+  {
+    if (n < 0 || m < 0) {
+      return 0;
+    }
+    else {
+      return n * m;
+    }
+  }
+}
+/* 
+public class Paper
+{
+  public static int paperWork(int n, int m) 
+  {
+    return (n < 0) || (m < 0) ? 0 : n * m;
+    //Happy Coding! ^_^
+  }
+}
+----------------------------------------------------------------
+
+import static java.lang.Math.max;
+
+public class Paper {
+
+  public static int paperWork(int n, int m) {
+    return max(m, 0) * max(n, 0);
+  }
+
+}
+----------------------------------------------------------------
+
+public class Paper
+{
+  public static int paperWork(int n, int m) 
+  {
+    return n>0&&m>0 ? m*n : 0;
+  }
+}
+----------------------------------------------------------------
+
+
+
+*/
