5d93bfe3ffa659d65cf6017955313c271a576ead
D Davis <davisdf@gmail.com>
2022-08-15 21:54:16 -0400

    Adding new solution

---
+++ YouOnlyNeedOne.java
@@ -0,0 +1,50 @@
+/*  8KYU  You only need one - Beginner
+
+You will be given an array a and a value x. 
+All you need to do is check whether the provided 
+array contains the value.
+Array can contain numbers or strings. X can be either.
+Return true if the array contains the value, false if not.
+
+*/
+
+public class Solution {
+
+    public static boolean check(Object[] a, Object x) {
+        for (int i = 0; i < a.length; i++) {
+          if (a[i] == x) {
+            return true;
+          }
+        }
+        return false;
+    }
+}
+/*---------------------------------------------------------------------
+import java.util.Arrays;
+
+public class Solution {
+
+    public static boolean check(Object[] a, Object x) {
+        return Arrays.asList(a).contains(x);
+    }
+
+} 
+-------------------------------------------------------------------------
+import java.util.Objects;
+
+public class Solution {
+
+    public static boolean check(Object[] a, Object x) {
+        for (Object o : a) {
+          if (Objects.equals(o, x)) {
+            return true;
+          }
+        }
+        return false;
+    }
+
+}
+---------------------------------------------------------------------------
+
+
+*/
