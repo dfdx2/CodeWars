ad52bc0fd1167b9f7a6014f89f57a5a1d9141989
D Davis <davisdf@gmail.com>
2022-08-27 19:48:02 -0400

    Adding new solution

---
+++ CountByX.java
@@ -0,0 +1,59 @@
+/*   8KYU  CodeWars  Count by X
+Create a function with two arguments that will return an array 
+of the first n multiples of x.
+
+Assume both the given number and the number of times to count 
+will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+Examples
+
+countBy(1,10) 
+
+// should return  {1,2,3,4,5,6,7,8,9,10}
+countBy(2,5)  // should return {2,4,6,8,10}
+
+*/
+
+public class Kata{
+    public static int[] countBy(int x, int n){
+      int[] result = new int[n];
+      for (int i = 0; i <= n - 1; i++) {
+        result[i] = x * (i + 1);
+      }
+      return result;
+    }
+  }
+/*-----------------------------------------------------------------
+
+import java.util.stream.IntStream;
+
+public class Kata{
+  public static int[] countBy(int x, int n){
+    
+    return IntStream.rangeClosed(1, n)
+      .map(i -> i * x)
+      .toArray();
+  }
+}
+-------------------------------------------------------------------
+public class Kata{
+  public static int[] countBy(int x, int n){
+    int[] el = new int[n];
+    for(int i = 0; i < n; i++)  el[i] = x * (i+1);
+    return el;
+  }
+}
+----------------------------------------------------------------
+import java.util.stream.IntStream;
+public class Kata{
+  public static int[] countBy(int x, int n){
+    return IntStream.rangeClosed(1, n * x).filter(i -> (i % x) == 0).toArray();
+  }
+}
+----------------------------------------------------------------------
+
+
+
+  */
