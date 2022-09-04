2e460bef53d6850666610a7e6c0e2044a3e138c4
D Davis <davisdf@gmail.com>
2022-09-04 13:43:46 -0400

    Added link to Kata

--- CountOddNumbersBelowN.java
+++ CountOddNumbersBelowN.java
@@ -1,5 +1,7 @@
 /*  8KYU CodeWars  Count Odd Numbers Below N 
 
+https://www.codewars.com/kata/59342039eb450e39970000a6
+
 Given a number n, return the number of positive odd numbers below n, EASY!
 
 Examples (Input -> Output)
