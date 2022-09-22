99a281ed404a3e92b6a221e33e7d8a1b0dcd5b08
D Davis <davisdf@gmail.com>
2022-09-22 10:04:21 -0400

    Updating Remove String Spaces Soultion

--- RemoveStringSpaces.java
+++ RemoveStringSpaces.java
@@ -1,6 +1,24 @@
+/*  8 KYU  CodeWars  Remove String Spaces
+
+Simple, remove the spaces from the string, 
+then return the resultant string.
+
+*/
+
 public class Kata {
-    public static String noSpace(final String x) {
-      String s1 = x.replaceAll("\\s", "");  
-      return s1;
+  public static String noSpace(final String x) {
+      return x.replaceAll("\\s", "");
+  }
+}
+
+/* -----------------------------------------------------------
+class Kata {
+    static String noSpace(final String x) {
+        return x.replace(" ", "");
     }
 }
+---------------------------------------------------------------
+
+
+
+*/
