f88b8b9ae53acd68d621bf4cc578235140fde2f9
D Davis <davisdf@gmail.com>
2022-09-27 16:01:42 -0400

    Update description for Binary to Decimal

--- BinToDecimal.java
+++ BinToDecimal.java
@@ -1,6 +1,13 @@
+/*  Binary To Decimal
+
+Take a binary input and convert it to a decimal
+
+
+*/
+
 public class BinToDecimal{
   public static int binToDecimal(String inp){
-    // your code here
+    
     int decimal = (Integer.parseInt(inp,2));
     return decimal;
   }
