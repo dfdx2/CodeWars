e421d5732d2a9369fcc21c59b914fc53306f19fa
D Davis <davisdf@gmail.com>
2022-09-28 14:59:16 -0400

    Updated description for Easy Logs

--- EasyLogs.java
+++ EasyLogs.java
@@ -1,8 +1,26 @@
+/* 8KYU Easy Logs 
+
+Add two logs with base X, with the value of A and B. 
+Example log A + log B where the base is X.
+
+*/
+
 public class EasyLogs {
 
   public static double logs(double x, double a, double b) {
-    // TODO add implementation
+    
     return (Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x));
+  }
+}
+/*-------------------------------------------------------------- 
+public class EasyLogs {
+
+  public static double logs(double x, double a, double b) {
+    return Math.log(a*b)/Math.log(x);
   }
 
 }
+-----------------------------------------------------------------
+
+
+*/
