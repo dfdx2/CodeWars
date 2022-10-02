893f7ea215998b84c5a303e9896af863f2c98de3
D Davis <davisdf@gmail.com>
2022-10-02 11:10:58 -0400

    Updated description for Counting Sheep problem

--- CountingSheep.java
+++ CountingSheep.java
@@ -1,3 +1,23 @@
+/* 
+Consider an array/list of sheep where some sheep may be 
+missing from their place. We need a function that counts 
+the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+  The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+
+*/
+
 import java.util.*;
 
 public class CountingSheep {
@@ -7,3 +27,61 @@ public class CountingSheep {
                   .count();
     }
   }
+
+/*--------------------------------------------------------------
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    int counter = 0;
+    for (Boolean present : arrayOfSheeps) {
+      if (present != null && present) {
+        counter += 1;
+      }
+    }
+    return counter;
+  }
+}
+------------------------------------------------------------------
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    return Collections.frequency(Arrays.asList(arrayOfSheeps), true);
+  }
+}
+-------------------------------------------------------------------
+import java.util.Arrays;
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    return (int) Arrays.stream(arrayOfSheeps).filter( e -> e != null && e == true).count();
+  }
+}
+---------------------------------------------------------------------
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    // TODO May the force be with you
+       if(arrayOfSheeps != null && arrayOfSheeps.length > 0){
+          int count = 0;
+         for(Boolean b : arrayOfSheeps){
+          if(b != null){
+              count += b ? 1 : 0;
+           }
+         }
+         return count;
+      }
+    return 0;
+    }
+}
+----------------------------------------------------------------------
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    // TODO May the force be with you
+    int total = 0;
+    for (Boolean arrayOfSheep : arrayOfSheeps) {
+      if (arrayOfSheep == null) continue;
+      if (arrayOfSheep == true) total += 1;
+    }
+    return total;
+  }
+}
+*/
