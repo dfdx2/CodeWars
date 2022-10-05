3382b1e7de2070eaaca669ad347faacfcb8792b3
D Davis <davisdf@gmail.com>
2022-10-05 18:59:55 -0400

    Updated Description for Simple Multiplication problem

--- SimpleMultiplication.java
+++ SimpleMultiplication.java
@@ -1,3 +1,10 @@
+/*  KYU 8 Simple Multiplication
+
+This kata is about multiplying a given number by 
+eight if it is an even number and by nine otherwise.
+
+*/
+
 public class SimpleMultiplication {
     public static int simpleMultiplication(int n) {
         //your code here
@@ -9,3 +16,13 @@ public class SimpleMultiplication {
       }
     }
 }
+/*--------------------------------------------------------
+public class Sid {
+    public static int simpleMultiplication (int n) {
+        return n % 2 == 0 ? n * 8 : n * 9;
+    }
+}
+
+
+
+*/
