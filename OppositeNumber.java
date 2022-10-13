8ac045c4e58cdf3d95dc6dc8adbb69b8a1949857
D Davis <davisdf@gmail.com>
2022-10-13 10:03:15 -0400

    Updated description for Opposite Number problem

--- OppositeNumber.java
+++ OppositeNumber.java
@@ -1,3 +1,16 @@
+/* 8 KYU CodeWars  Opposite Number
+
+Very simple, given an integer or a floating-
+point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
 public class OppositeNumber
     {
         public static int opposite(int number)
@@ -6,3 +19,14 @@ public class OppositeNumber
           return -number;
         }
     }
+/*------------------------------------------------------
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            return number == 0 ? number : -number;
+        }
+    }
+-------------------------------------------------------
+
+*/
