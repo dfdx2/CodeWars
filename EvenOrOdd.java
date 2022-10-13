962a66291f03bc5313a9ec1345bebcb580833934
D Davis <davisdf@gmail.com>
2022-10-12 20:20:58 -0400

    Updated solution for Even or Odd

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,3 +1,18 @@
+/*   8KYU CodeWars  Even or Odd
+
+Create a function that takes an integer as an argument and 
+returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        //Place code here
+      String result = (number % 2 == 0) ? "Even" : "Odd"; 
+      return result;
+    }
+}
+
+/*-------------------------------------------------------------------
 public class EvenOrOdd {
     public static String even_or_odd(int number) {
         //Place code here
@@ -8,3 +23,12 @@ public class EvenOrOdd {
         }
     }
 }
+--------------------------------------------------------------------------
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return (number%2 == 0) ? "Even" : "Odd";
+    }
+}
+---------------------------------------------------------------------
+
+*/
