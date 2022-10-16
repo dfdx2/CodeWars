7125edc9eee24665aa1fbf63e05e94e9ad2f1cab
D Davis <davisdf@gmail.com>
2022-10-16 13:41:39 -0400

    Updated solution for Area Or Perimeter problem

--- AreaOrPerimeter.java
+++ AreaOrPerimeter.java
@@ -1,10 +1,30 @@
-/*
-Defineed as perimeter if the length is equal to width
-Defined as area if length is different than width
+/*  8 KYU CodeWars  Area or Perimeter
 
+You are given the length and width of a 
+4-sided polygon. The polygon can either be 
+a rectangle or a square.
+If it is a square, return its area. If it 
+is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will 
+assume that it is a square if its length and 
+width are equal, otherwise it is a rectangle.
 
 */
-
+public class Solution {
+  public static int areaOrPerimeter (int l, int w) {
+      // code away...
+    int result;
+    return result = (l != w) ? ((l * 2) + (w * 2)) : l * w;
+  }
+}
+/*
+--------------------------------------------------------------
+previous solution
 public class AreaOrPerimeter {
     public static int areaOrPerimeter (int l, int w) {
         // code away...
@@ -15,3 +35,12 @@ public class AreaOrPerimeter {
       
     }
 }
+----------------------------------------------------------------
+
+public class Solution {
+    public static int areaOrPerimeter (int l, int w) {
+        return l == w ? l * w : (l + w) * 2;
+    }
+}
+
+*/
