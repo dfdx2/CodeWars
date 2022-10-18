be29208e03c2e1877d852187cfaddc67d98078ff
D Davis <davisdf@gmail.com>
2022-10-18 16:31:56 -0400

    Updated solution for Return Negative Kata

--- ReturnNegative.java
+++ ReturnNegative.java
@@ -1,3 +1,30 @@
+/*  8KYU CodeWars Return Negative
+
+In this simple assignment you are given a number and have 
+to make it negative. But maybe the number is already negative?
+
+Examples
+Kata.makeNegative(1);  // return -1
+Kata.makeNegative(-5); // return -5
+Kata.makeNegative(0);  // return 0
+Notes
+The number can be negative already, in which case no change 
+is required.
+Zero (0) is not checked for any specific sign. Negative 
+zeros make no mathematical sense.
+
+*/
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    
+    return (x > 0) ? -x : x;
+    
+  }
+  
+}
+/*------------------------------------------------------------
+old solution
 public class ReturnNegative {
 
   public static int makeNegative(final int number) {
@@ -11,3 +38,22 @@ public class ReturnNegative {
   }
   
 }
+---------------------------------------------------------------
+import static java.lang.Math.abs;
+
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    return -abs(x);
+  }
+  
+}
+-----------------------------------------------------------------
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    return Math.abs(x) * -1;
+  }
+  
+}
+*/
