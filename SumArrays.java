148abe2cd38854365be987d6b50463374761c5f7
D Davis <davisdf@gmail.com>
2022-10-20 10:04:39 -0400

    Updated solution for Sum of Arrays

--- SumArrays.java
+++ SumArrays.java
@@ -23,7 +23,7 @@ What We're Testing
 We're testing basic loops and math operations. This is for beginners who are 
 just learning loops and math operations. Advanced users may find this 
 extremely easy and can easily write this in one line.
-
+Can you make it better?
 */
 
 public class SumArray {
