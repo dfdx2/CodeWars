b2d11658bf05d39a97b1376a2fdf01b9cfa6d9eb
D Davis <davisdf@gmail.com>
2022-10-22 18:38:26 -0400

    Adding solution for find the smallest Integer

--- FindTheSmallestIntegerInTheArray.java
+++ FindTheSmallestIntegerInTheArray.java
@@ -17,3 +17,48 @@ public class SmallestIntegerFinder {
 
     }
 }
+/*------------------------------------------------------------------
+import java.util.stream.IntStream;
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        return IntStream.of(args).min().getAsInt();
+    }
+}
+-------------------------------------------------------------------
+import java.util.Arrays;
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+      int min = args[0];
+      
+       for(int i = 1; i < args.length; i++){
+         if(min > args[i]) min = args[i];
+       }
+       
+      return min;
+    }
+}
+---------------------------------------------------------------------
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        int smallest = Integer.MAX_VALUE;
+        for (int i : args)
+          smallest = (smallest>i) ? i : smallest;
+        return smallest;
+    }
+}
+--------------------------------------------------------------------
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        int min = args[0];
+        for (int i = 1; i < args.length; i++) {
+           if (min > args[i])
+               min = args[i];
+        }
+        return min;
+    }
+}
+--------------------------------------------------------------------
+
+*/
