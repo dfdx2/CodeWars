4d6416668b14ec8fe1f8d3a18e53c9ac9bab6ace
D Davis <davisdf@gmail.com>
2022-10-25 13:42:46 -0400

    Updated solution for Count Odd N Numbers

--- CountOddNumbersBelowN.java
+++ CountOddNumbersBelowN.java
@@ -1,7 +1,5 @@
 /*  8KYU CodeWars  Count Odd Numbers Below N 
 
-https://www.codewars.com/kata/59342039eb450e39970000a6
-
 Given a number n, return the number of positive odd numbers below n, EASY!
 
 Examples (Input -> Output)
@@ -21,9 +19,3 @@ public class Kata {
 
   }
 
-}
-
-    return result;
-  }
-
-}
