b678589e8a5434fddcdaa88ae7be18228943dfcc
D Davis <davisdf@gmail.com>
2022-10-26 09:52:45 -0400

    Updated solution for you cant code under pressure

--- YouCantCodeUnderPressure1.java
+++ YouCantCodeUnderPressure1.java
@@ -1,7 +1,29 @@
+/*  8KYU CodeWars  You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the 
+integer and return it.
+
+
+*/
+
+
 class YouCantCodeUnderPressure1 {
   public static int doubleInteger(int i) {
     // Double the integer and return it!
-    i = i * 2;
-    return i;
+    return i * 2;
   }
 }
+/*--------------------------------------------------------------
+class Java {
+  public static int doubleInteger(int i) {
+    
+    return (i * 4)/2;
+  }
+}
+------------------------------------------------------------------
+class Java {
+  public static int doubleInteger(int i) {
+    return i += i;
+  }
+}
+*/
