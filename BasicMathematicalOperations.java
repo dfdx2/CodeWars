6e5a349326c598a4fdf98f74f910d47ee360b264
D Davis <davisdf@gmail.com>
2022-10-31 14:06:00 -0400

    Updated solution for Basic Math Operations

--- BasicMathematicalOperations.java
+++ BasicMathematicalOperations.java
@@ -1,8 +1,108 @@
-/*
-This Kata goes over the basic math operations of 
-addition, substraction, multiplication and division.
+/*  8KYU CodeWars  Basic Mathematical Operations
+
+Your task is to create a function that does four basic 
+mathematical operations.
+
+The function should take three arguments - 
+operation(string/char), value1(number), value2(number).
+The function should return result of numbers after 
+applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
 
 */
+
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    if (op == "+") {
+      return v1 + v2;
+    }
+    else if (op == "-") {
+      return v1 - v2;
+    }
+    else if (op == "*") {
+      return v1 * v2;
+    }
+    else {
+      return v1 / v2;
+    }
+    
+  }
+}
+/*-----------------------------------------------------------------
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+  switch (op) {
+    case "-":
+      return v1 - v2;
+    case "+":
+      return v1 + v2;
+    case "*":
+      return v1 * v2;
+    case "/": {
+      if (v2 == 0)
+        throw new IllegalArgumentException("Division by zero");
+      return v1 / v2;
+    }
+    default:
+      throw new IllegalArgumentException("Unknown operation: " + op);
+    }
+  }
+}
+-----------------------------------------------------------------
+public class BasicOperations{
+  public static Integer basicMath(String symbol, int x, int y){
+    switch (symbol){
+      case "+": return x+y;
+      case "-": return x-y;
+      case "*": return x*y;
+      case "/": return x/y;
+    }
+    return 0;
+  }
+}
+--------------------------------------------------------------------
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    return (op=="+") ? (v1+v2) : (op=="-") ? (v1-v2) : (op=="*") ? (v1*v2) : (v2==0) ? null : (v1/v2);
+    
+  }
+}
+--------------------------------------------------------------------
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    int valor = 0;
+    if("+".equals(op)) valor = v1 + v2;
+    if("-".equals(op)) valor = v1 - v2;
+    if("*".equals(op)) valor = v1 * v2;
+    if("/".equals(op)) valor = v1 / v2;
+    return valor;
+  }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+/* Old solution
 public class BasicOperations
 {
   public static Integer basicMath(String op, int v1, int v2)
@@ -22,3 +122,4 @@ public class BasicOperations
     }
   }
 }
+*/
