99d20aab473cdb458a0714ced42e4289a93a804e
D Davis <davisdf@gmail.com>
2022-11-05 12:08:30 -0400

    Updated Solution for Remove Exclamation Marks

--- RemoveExclamtionMarks.java
+++ RemoveExclamtionMarks.java
@@ -1,6 +1,63 @@
+/*  8KYU CodeWars Remove Exclamation Marks
+
+Write function RemoveExclamationMarks which removes 
+all exclamation marks from a given string.
+
+*/
+
 class RemoveExclamtionMarks {
     static String removeExclamationMarks(String s) {
-      s = s.replaceAll("!","");
-        return s;
+      return s.replaceAll("!","");
     }
 }
+/*-------------------------------------------------------
+class Solution {
+    static String removeExclamationMarks(String s) {
+        return (s == null) ? null : s.replace("!", "");
+    }
+}
+-----------------------------------------------------------
+class Solution {
+     static String removeExclamationMarks(String s) {
+        char[] arr = s.toCharArray();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != '!') {
+                builder.append(arr[i]);
+            }
+        }
+
+        return builder.toString();
+    }
+}
+------------------------------------------------------------
+import java.util.stream.*;
+class Solution {
+    static String removeExclamationMarks(String s) {
+        return Stream.of(s.split(""))
+                 .filter(l -> !l.equals("!"))
+                 .collect(Collectors.joining(""));
+    }
+}
+-------------------------------------------------------------
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+class Solution {
+    static String removeExclamationMarks(String s) { 
+        
+    return IntStream.range(0, s.length()).filter(i -> s.charAt(i) != '!').mapToObj(i -> "" + s.charAt(i))
+        .collect(Collectors.joining(""));
+    }
+}
+---------------------------------------------------------------
+class Solution {
+    static String removeExclamationMarks(String s) {
+        String [] sen = s.split("!");
+        return String.join("", sen);
+    }
+}
+
+*/
