303de76a340d74e0a2952c273f5f829c33da4bac
D Davis <davisdf@gmail.com>
2022-11-06 09:21:31 -0500

    Updated solution for Are You Playing Banjo

--- AreYouPlayingBanjo.java
+++ AreYouPlayingBanjo.java
@@ -4,7 +4,6 @@ Create a function which answers the question
 "Are you playing banjo?".
 If your name starts with the letter "R" or 
 lower case "r", you are playing banjo!
-So your solution has to account for that.
 
 The function takes a name as its only argument, 
 and returns one of the following strings:
@@ -14,7 +13,14 @@ name + " does not play banjo"
 Names given are always valid strings.
 
 */
+public class Banjo {
+  public static String areYouPlayingBanjo(String name) {
+    // Program me!
+    return (name.charAt(0) == 'r' || name.charAt(0) == 'R') ? name + " plays banjo" : name + " does not play banjo";
+   }
+}
 
+/* OLD SOLUTION
 public class Banjo {
   public static String areYouPlayingBanjo(String name) {
     // Program me!
