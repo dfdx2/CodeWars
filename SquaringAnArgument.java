5b1532238dc02b1f939af5835fddfda02c90f36c
D Davis <davisdf@gmail.com>
2022-11-09 12:30:06 -0500

    Solution for Squaring an Argument

---
+++ SquaringAnArgument.java
@@ -0,0 +1,22 @@
+/* 8KYU Function 2 - Squaring An Argument
+
+Now you have to write a function that takes 
+an argument and returns the square of it.
+
+*/
+
+public class Kata
+ {
+  public static int square(int n){
+    return n * n;
+   }
+ }
+/*------------------------------------------------------------
+public class Kata
+ {
+  public static int square(int n){
+    return (int) Math.pow(n, 2);
+  }
+ }
+---------------------------------------------------------------
+ */
