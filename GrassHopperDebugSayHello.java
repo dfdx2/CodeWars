7c15ac798434d6284f059c4f1930730861041fa2
D Davis <davisdf@gmail.com>
2022-11-14 14:08:17 -0500

    Updated solution for Say Hello

--- GrassHopperDebugSayHello.java
+++ GrassHopperDebugSayHello.java
@@ -1,3 +1,18 @@
+/* 8KYU CodeWars  GRASSHOPPER - DEBUG SAY HELLO
+
+Debugging sayHello function
+
+The starship Enterprise has run into some problem when 
+creating a program to greet everyone as they come aboard. 
+It is your job to fix the code and get the program working 
+again!
+
+Example output:
+
+Hello, Mr. Spock
+
+*/
+
 public class GrassHopper {
 
     public static String sayHello(String name) {
