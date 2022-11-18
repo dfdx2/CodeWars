46e7163e0ea37ee1843f1797c89842e3c81fbc1b
D Davis <davisdf@gmail.com>
2022-11-18 17:15:04 -0500

    Updated solution for Twice As Old

--- TwiceAsOld.java
+++ TwiceAsOld.java
@@ -1,9 +1,38 @@
+/*  8KYU CodeWars  Twice As Old
+
+Your function takes two arguments:
+
+1. current father's age (years)
+2. current age of his son (years)
+
+Сalculate how many years ago the father was twice as old 
+as his son (or in how many years he will be twice as old). 
+The answer is always greater or equal to 0, no matter if 
+it was in the past or it is in the future.
+
+*/
+
 public class TwiceAsOld{
 
   public static int TwiceAsOld(int dadYears, int sonYears){
-    //TODO: Add code here
     return Math.abs(sonYears * 2 - dadYears);
-  
   }
 
 }
+/*-----------------------------------------------------------
+public class TwiceAsOld{
+
+  public static int TwiceAsOld(int dadYears, int sonYears){
+    int diff = dadYears - sonYears * 2;
+    return diff > 0 ? diff : -diff;
+  }
+
+}
+-------------------------------------------------------------
+
+
+
+
+
+
+*/
