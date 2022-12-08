77adcf6bfa3e5928d2e6d84a9dcd9cfe98a4f68c
D Davis <davisdf@gmail.com>
2022-12-08 12:50:00 -0500

    Solution for Keep Up The Hoop

---
+++ KeepUpTheHoop.java
@@ -0,0 +1,49 @@
+/* 8KYU CodeWars keep up the hoop
+
+Alex just got a new hula hoop, he loves it but feels 
+discouraged because his little brother is better than 
+him  Write a program where Alex can input (n) how many 
+times the hoop goes round and it will return him an 
+encouraging message :)
+If Alex gets 10 or more hoops, return the string 
+"Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string 
+"Keep at it until you get it".
+
+*/
+
+public class HelpAlex{
+  public static String hoopCount(int n){
+   return (n >= 10) ? "Great, now move on to tricks" : "Keep at it until you get it";
+  }
+}
+/*---------------------------------------------------------------
+import java.util.*;
+
+public class HelpAlex{
+  public static String hoopCount(int n){
+    Map<Boolean, String> phrase = new HashMap<>();
+    phrase.put(true, "Great, now move on to tricks");
+    phrase.put(false, "Keep at it until you get it");
+    
+   return phrase.get(n >= 10);
+  }
+}
+-------------------------------------------------------------------
+public class HelpAlex{
+  public static String hoopCount(int n){
+   String phrase = "";
+    if(n >= 10){
+      phrase = "Great, now move on to tricks";
+    }else{
+      phrase = "Keep at it until you get it";
+    }
+    return phrase;
+  }
+}
+-------------------------------------------------------------------
+
+
+
+
+*/
