562b7ceec38df84e50c0ee7199fe79e7936a6c3e
D Davis <davisdf@gmail.com>
2022-12-23 12:47:40 -0500

    Updated solution for Jennys Secret Message

--- JennysSecretMessage.java
+++ JennysSecretMessage.java
@@ -1,3 +1,47 @@
+/* 8KYU CodeWars Jenny's Secret Message
+
+Jenny has written a function that returns a greeting for a user. 
+However, she's in love with Johnny, and would like to greet him 
+slightly different. She added a special case to her function, but 
+she made a mistake.
+
+Can you help her?
+
+*/
+
+public class Greeter {
+  public static String greet(String name) {
+    
+    if (name.equals("Johnny")){
+      return "Hello, my love!";
+    }
+    
+    return name.format("Hello, %s!", name);
+  }
+}
+/*-----------------------------------------------------------------
+public class Greeter {
+  public static String greet(String name) {
+    return "Hello, " + (name == "Johnny" ?  "my love" : name) + "!";
+  }
+}
+------------------------------------------------------------------
+public class Greeter {
+  public static String greet(String name) {
+    if(!name.equals("Johnny")){
+      return String.format("Hello, %s!", name);
+    } else {
+      return "Hello, my love!";
+    }
+  }
+}
+------------------------------------------------------------------
+public class Greeter {
+  public static String greet(String name) {
+    return name.equals("Johnny")? "Hello, my love!" : String.format("Hello, %s!", name);
+  }
+}
+------------------------------------------------------------------
 public class Greeter {
   public static String greet(String name) {
     
@@ -11,3 +55,6 @@ public class Greeter {
     }
   }
 }
+
+
+*/
