3416cbd893ecb0a8bb9a4324453418030f356c27
D Davis <davisdf@gmail.com>
2022-12-25 08:51:31 -0500

    Updated solution for Grade Book

--- GradeBook.java
+++ GradeBook.java
@@ -17,6 +17,29 @@ negative values or values greater than 100.
 */
 public class GrassHopper {
 
+  public static char getGrade(int s1, int s2, int s3) {
+
+    int average = (s1 + s2 + s3) / 3;
+    if (average >= 0 && average < 60) {
+      return 'F';
+    }
+    else if (average >= 60 && average < 70) {
+      return 'D';
+    }
+    else if (average >= 70 && average < 80) {
+      return 'C';
+    }
+    else if (average >= 80 && average < 90) {
+      return 'B';
+    }
+    else {
+      return 'A';
+    }
+  }
+}
+/* -------------------------------------------------------------------------------------------------------------------
+public class GrassHopper {
+
     public static char getGrade(int s1, int s2, int s3) {
       char A = 'A';
       char B = 'B';
@@ -41,7 +64,7 @@ public class GrassHopper {
 
     }
 }
- /*---------------------------------------------------------------------------------------
+ ---------------------------------------------------------------------------------------
  public class GrassHopper {
 
     public static char getGrade(int s1, int s2, int s3) {
