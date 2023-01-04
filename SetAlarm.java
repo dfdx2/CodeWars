d54cc2b7746c8bcc8091ac409a35b47c05a9f35a
D Davis <davisdf@gmail.com>
2023-01-03 19:33:03 -0500

    Adding solution for Set Alarm problem

---
+++ SetAlarm.java
@@ -0,0 +1,66 @@
+/* 8KYU L1: Set Alarm
+
+Write a function named setAlarm which receives two parameters. 
+The first parameter, employed, is true whenever you are 
+employed and the second parameter, vacation is true whenever 
+you are on vacation.
+
+The function should return true if you are employed and not 
+on vacation (because these are the circumstances under which 
+you need to set an alarm). It should return false otherwise. 
+
+Examples:
+
+setAlarm(true, true) -> false
+setAlarm(false, true) -> false
+setAlarm(false, false) -> false
+setAlarm(true, false) -> true
+
+*/
+
+public class Alarm {
+  
+  public static boolean setAlarm(boolean employed, boolean vacation) {
+    return (employed && !vacation) ? true : false;
+  }
+}
+
+/*-----------------------------------------------------------------
+public class Alarm {
+  
+  public static boolean setAlarm(boolean employed, boolean vacation) {
+  if (employed == true && vacation == true) {
+  return false;
+  } else if (employed == false && vacation == true) {
+  return false;
+  } else if (employed == false && vacation == false) {
+  return false;
+  } else {
+  return true;
+  }
+  }
+}
+--------------------------------------------------------------------
+public class Alarm {
+  
+  public static boolean setAlarm(boolean employed, boolean vacation) {
+    if (employed ^ vacation) 
+      return employed;
+    else;
+      return false;
+  
+  }
+
+}
+-------------------------------------------------------------------
+public class Alarm {
+  
+  public static boolean setAlarm(boolean employed, boolean vacation) {
+    if(employed && !vacation)
+      return true;
+    return false;
+  }
+
+}
+
+*/
