08018037658826e1c9be99189ce6e642324c96fe
D Davis <davisdf@gmail.com>
2023-01-05 17:36:10 -0500

    Updated solution for personalized message

--- GrasshopperPersonalizedMessage.java
+++ GrasshopperPersonalizedMessage.java
@@ -1,3 +1,22 @@
+/* 8KYU CodeWars  Grasshopper - Personalized Message
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+*/
+
+class Kata {
+  static String greet(String name, String owner) {
+    return (owner.equalsIgnoreCase(name)) ? "Hello boss" : "Hello guest";
+  }
+}
+
+/*------------------------------------------------------------------
 class GrasshopperPersonalizedMessage {
     static String greet(String name, String owner) {
         // Add code here
@@ -9,3 +28,24 @@ class GrasshopperPersonalizedMessage {
       }
     }
 }
+--------------------------------------------------------------------
+class Kata {
+    static String greet(String name, String owner) {
+        return "Hello " + (name.equals(owner) ? "boss":"guest");
+    }
+}
+---------------------------------------------------------------------
+class Kata {
+    static String greet(String name, String owner) {
+      String a = "Hello boss" ; 
+      String b = "Hello guest";
+      if (name.matches(owner)){
+          return a;
+          }
+          else {
+            return b;
+        }
+    }
+}
+
+*/
