3ca2f4362a8a150f5fa6516439714312f0b6ff1b
D Davis <davisdf@gmail.com>
2023-01-10 17:28:10 -0500

    Updated solution for Small Enough

--- SmallEnough.java
+++ SmallEnough.java
@@ -19,3 +19,79 @@ public class Kata
     return true;
   }
 }
+/*-----------------------------------------------------------
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit) {
+    return java.util.Arrays.stream(a).noneMatch(i -> i>limit);
+  }
+}
+--------------------------------------------------------------
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit)
+	  {
+	  for (int number : a) {
+		  if (number > limit) return false;
+	  }
+	    return true;
+	  }
+}
+---------------------------------------------------------------
+import java.util.stream.*;
+
+public class Kata {
+
+  public static boolean smallEnough(int[] a, int limit) {
+    return IntStream.of(a).allMatch(x->x<=limit);
+  }
+  
+}
+----------------------------------------------------------------
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit) {
+    
+    boolean smaller = true;
+    for (int e : a) {
+      if(e > limit){ 
+        smaller=false;
+      }
+    }
+    return smaller;
+  }
+}
+--------------------------------------------------------------
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit)
+  {
+    return java.util.Arrays.stream(a).max().orElse(0) <= limit;
+  }
+}
+--------------------------------------------------------------
+import java.util.Arrays;
+
+public class Kata
+{
+    public static boolean smallEnough(int[] a, int limit)
+    {
+        return Arrays.stream(a)
+                .filter(e -> e <= limit)
+                .count() == a.length;
+    }
+}
+------------------------------------------------------------
+import java.util.Arrays;
+
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit)
+  {
+    return Arrays.stream(a).allMatch(i -> i <= limit);
+  }
+}
+
+
+
+*/
