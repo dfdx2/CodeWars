15c2ceeed3edf1c7d33b51fb2ab45986b302db26
D Davis <davisdf@gmail.com>
2023-01-15 09:48:45 -0500

    Updated solution for Predict Your Age

--- PredictYourAge.java
+++ PredictYourAge.java
@@ -22,10 +22,10 @@ Simply resubmit if that happens to you.
 public class Solution {
     public static int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
         // your code goes here
-      int sum1 = (age1 * age1) + (age2 * age2) + (age3 * age3) + (age4 * age4) + (age5 * age5) + (age6 * age6) + (age7 * age7) + (age8 * age8);
-      double sum2 = Math.sqrt(sum1);
-      double sum3 = sum2 / 2;
-      return (int)sum3;
+      int result = (age1 * age1) + (age2 * age2) + (age3 * age3) + (age4 * age4) + (age5 * age5) + (age6 * age6) + (age7 * age7) + (age8 * age8);
+      double x = Math.sqrt(result);
+      int y = (int) x / 2;
+      return y;
     }
 }
 /* 
