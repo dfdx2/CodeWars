8433941f48e272656e38054962df67eda781a17c
D Davis <davisdf@gmail.com>
2023-01-25 16:12:57 -0500

    Updated solution for Welcome To The City

--- WelcomeToTheCity.java
+++ WelcomeToTheCity.java
@@ -1,10 +1,86 @@
+/* 8KYU CodeWars Welcome To The City
 
-/*Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state
- to welcome a person. Note that name will be an array consisting of one or more values 
- that should be joined together with one space between each, and the length of the name 
- array in test cases will vary.
+Create a method sayHello/say_hello/SayHello that takes as 
+input a name, city, and state to welcome a person. Note that 
+name will be an array consisting of one or more values that 
+should be joined together with one space between each, and 
+the length of the name array in test cases will vary.
+
+Example:
+
+sayHello(new String[]{"John", "Smith"}, "Phoenix", "Arizona")
+This example will return the string Hello, John Smith! 
+Welcome to Phoenix, Arizona!
+
 */
 
+public class Hello{
+  public String sayHello(String [] name, String city, String state) {
+    
+    String x = "";
+    for (int i = 0; i < name.length; i++) {
+      x += " " + name[i];
+    }
+    return "Hello," + x + "!" + " Welcome to " + city + ", " + state + "!";
+  }
+}
+/*----------------------------------------------------------------------
+public class Hello{
+  public String sayHello(String[] name, String city, String state){
+    return String.format("Hello, %s! Welcome to %s, %s!",String.join(" ", name),city,state);
+  }
+}
+---------------------------------------------------------------------
+public class Hello{
+  public String sayHello(String [] name, String city, String state){
+    String fullName = "";
+    for (String nam : name){
+        fullName += " " + nam;
+    }
+    return String.format("Hello,%s! Welcome to %s, %s!", fullName, city, state);
+  }
+}
+----------------------------------------------------------------------
+public class Hello{
+  public String sayHello(String [] name, String city, String state){
+    String fullName = String.join(" ", name);
+    return String.format("Hello, %s! Welcome to %s, %s!", fullName, city, state);
+  }
+}
+----------------------------------------------------------------------
+public class Hello{
+  public String sayHello(String [] name, String city, String state){
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello,");
+    for(String n : name){
+      sb.append(" ");
+      sb.append(n);
+    }
+    sb.append("! Welcome to ");
+    sb.append(city);
+    sb.append(", ");
+    sb.append(state);
+    sb.append("!");
+    return sb.toString();
+  }
+}
+--------------------------------------------------------------------
+import org.apache.commons.lang3.StringUtils;
+
+public class Hello{
+  public String sayHello(String [] name, String city, String state){
+    //code here
+     String str = StringUtils.join(name," ");
+    return "Hello, " + str +"! Welcome to " +city +", "+ state + "!";
+  }
+}
+---------------------------------------------------------------------
+class Hello {
+  String sayHello(String[] name, String city, String state) {
+    return "Hello, " + String.join(" ", name) + "! Welcome to " + city + ", " + state + "!";
+  }
+}
+----------------------------------------------------------------------
 public class We!comeToTheCity{
     public String sayHello(String [] name, String city, String state){
       //code here
@@ -27,3 +103,6 @@ public class Hello{
     return ("Hello, " + str + str1 + tres);
   }
 } */
+
+
+*/
