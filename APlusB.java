c00901d75f21af047b32ac74b2bd0e7f68b22696
D Davis <davisdf@gmail.com>
2023-01-26 18:15:46 -0500

    Updated Solution for A Plus B

--- APlusB.java
+++ APlusB.java
@@ -1,9 +1,19 @@
-/*  
+/* 8KYU CodeWars A + B
 
-Return the sum of 2 bytes
-converted the return type to byte to pass the kata
+Vasya Pupkin just started learning Java and C#. At first, 
+he decided to write simple program that was supposed to 
+sum up two small numbers (numbers and their sum fit in 
+a byte), but it didn't work. Vasya was too sad to find 
+out what is wrong. Help him to correct the mistake.
 
 */
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        int c = a + b;
+        return (byte) c;
+    }
+}
+/*---------------------------------------------------------------------
 
 public class FirstClass {
     public static byte sum (byte a, byte b) {
@@ -11,3 +21,37 @@ public class FirstClass {
         return c;
     }
 }
+---------------------------------------------------------------------
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        // JLS states that operations on bytes/shorts are promoted to ints.
+        // a downcast is therefore needed
+        byte c = (byte)(a + b);
+        return c;
+    }
+}
+----------------------------------------------------------------------
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        return (byte)(a + b);
+        
+    }
+}
+---------------------------------------------------------------------
+class FirstClass {
+  static byte sum(byte a, byte b) {
+    return a += b;
+  }
+}
+---------------------------------------------------------------------
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+     return new Byte(Integer.toString(a+b));
+    }
+}
+---------------------------------------------------------------------
+
+
+
+
+*/
