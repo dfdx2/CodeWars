091c6ad8f1942a9a92035d98984bc6dcba15369c
D Davis <davisdf@gmail.com>
2023-01-29 11:21:30 -0500

    Updated solution for Offspring

--- Offspring.java
+++ Offspring.java
@@ -1,3 +1,31 @@
+/* 8KYU CodeWars Determine Offspring Sex Based On Genes XX and XY 
+
+The male gametes or sperm cells in humans and other mammals are 
+heterogametic and contain one of two types of sex chromosomes. 
+They are either X or Y. The female gametes or eggs however, contain 
+only the X sex chromosome and are homogametic.
+
+The sperm cell determines the sex of an individual in this case. 
+If a sperm cell containing an X chromosome fertilizes an egg, the 
+resulting zygote will be XX or female. If the sperm cell contains 
+a Y chromosome, then the resulting zygote will be XY or male.
+
+Determine if the sex of the offspring will be male or female based 
+on the X or Y chromosome present in the male's sperm.
+
+If the sperm contains the X chromosome, return "Congratulations! 
+You're going to have a daughter."; If the sperm contains the Y 
+chromosome, return "Congratulations! You're going to have a son.";
+
+*/
+public class Kata {
+  public static String chromosomeCheck(String sperm) {
+    // Your code goes here. Have fun!
+    return (sperm == "XX") ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son.";
+  }
+}
+
+/*-----------------------------------------------------------------
 public class Offspring {
   public static String chromosomeCheck(String sperm) {
     // Your code goes here. Have fun!
@@ -9,3 +37,35 @@ public class Offspring {
     }
   }
 }
+---------------------------------------------------------------------
+
+public class Kata {
+    public static String chromosomeCheck(String sperm) {
+        return "Congratulations! You're going to have a " + (sperm.contains("Y") ? "son." : "daughter.");
+    }
+}
+----------------------------------------------------------------------
+public class Kata {
+  public static String chromosomeCheck(String sperm) {
+   
+     switch(sperm) {
+         case "XY":
+         return "Congratulations! You're going to have a son.";
+         case "XX":
+         return "Congratulations! You're going to have a daughter.";
+         default:
+         return "Chromosomes error!!!";
+     }
+  }
+}
+---------------------------------------------------------------------
+public class Kata {
+
+  public static String chromosomeCheck(String sperm) {
+    String child = sperm.equals("XY") ? "son" : "daughter";
+    return "Congratulations! You're going to have a " + child + ".";
+  }
+}
+
+
+*/
