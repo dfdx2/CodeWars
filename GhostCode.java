6be5af8b710066ad7fd46df412b72434a7ff2bd6
D Davis <davisdf@gmail.com>
2023-02-05 15:39:49 -0500

    Updated solution for Ghost Code

--- GhostCode.java
+++ GhostCode.java
@@ -1,9 +1,47 @@
+/* 8KYU CodeWars Ghost Code
+
+Trollencio changed my code! can you fix it? My name is not "dumb!" o( TT_TT )o
+
+Instructions: Fix the code
+
+Input
+Javatlacati
+Output
+Hello my name is Javatlacati
+Input
+""
+Output
+Hello world!
+
+*/
+
 public class GhostCode{
   public static String helloName(final String name){
   // 
+    if(name == null || name == "dumb" || name.length() == 0) {
+      return "Hello world!";
+    }
+    else {
+      return "Hello my name is " + name;
+    }
+    
+  }
+}
+/*----------------------------------------------------------------------------
+public class GhostCode{
+  public static String helloName(final String name){
+    return (name==null || name.equals("")) ?  "Hello world!" : "Hello my name is " + name;
+  }
+}
+---------------------------------------------------------------------------------
+public class GhostCode{
+  public static String helloName(final String name){
     if(name == null || name.equals(""))
       return "Hello world!";
     else
       return "Hello my name is " + name;
   }
 }
+
+
+*/
