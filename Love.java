90c60f78c95518dceea69adf28d51a722d0bcb30
D Davis <davisdf@gmail.com>
2023-02-19 11:24:37 -0500

    Updated solution for Love

--- Love.java
+++ Love.java
@@ -14,7 +14,15 @@
         of a given number of petals, where nb_petals > 0.
  */
 
-public class Love {
+ public class Sid {
+      public static String howMuchILoveYou(int nb_petals) {
+        
+        return (nb_petals % 6 == 1) ? "I love you" : (nb_petals % 6 == 2) ? "a little" : (nb_petals % 6 == 3) ? "a lot" : (nb_petals % 6 == 4) ? "passionately" : (nb_petals % 6 == 5) ? "madly" : "not at all";
+     
+      }
+  }
+/*------------------------------------------------------------------------------------------
+ public class Love {
     public static String howMuchILoveYou(int nb_petals) {
     
     //your code here :)
@@ -34,3 +42,73 @@ public class Love {
         return ("not at all");
   }
 }
+-----------------------------------------------------------------------------------------------------
+public class Sid {
+    public static String howMuchILoveYou(int nb_petals) {
+    
+      String[] arr ={"not at all", "I love you",  "a little", "a lot", "passionately", "madly"};
+    
+      return arr[nb_petals%6];
+    }
+}
+---------------------------------------------------------------------------------------------------
+public class Sid {
+    public static String howMuchILoveYou(int nb_petals) {
+    
+    //your code here :)
+      switch (nb_petals % 6){
+            case 1:
+                return "I love you";
+            case 2:
+                return "a little";
+            case 3:
+                return "a lot";
+            case 4:
+                return "passionately";
+            case 5:
+                return "madly";
+            case 0:
+                return "not at all";
+                default:
+                return "";
+        }
+    }
+}
+--------------------------------------------------------------------------------------------
+class Sid {
+    private static final String[] phrases = {
+        "I love you","a little","a lot","passionately","madly","not at all"
+    };
+
+    static String howMuchILoveYou(final int nbPetals) {  
+        return phrases[(nbPetals - 1) % phrases.length];
+    }
+}
+------------------------------------------------------------------------------------------
+public class Sid {
+    public static String howMuchILoveYou(int nb_petals) {
+        int result = nb_petals;
+        while (result > 6) {
+            result -= 6;
+        }
+
+        switch (result){
+            case 1:return "I love you";
+            case 2:return "a little";
+            case 3:return "a lot";
+            case 4:return "passionately";
+            case 5:return "madly";
+            case 6:return "not at all";
+            default:return "";
+        }
+    }
+}
+---------------------------------------------------------------------------------------
+public class Sid {
+    public static String howMuchILoveYou(int n) {
+      return new String[]{"not at all", "I love you", "a little", "a lot", "passionately", "madly"}[n % 6];
+    }
+}
+
+
+*/
