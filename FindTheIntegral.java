2d6ca1b0cb9ddbdd7b8e9aa589f63b300843987b
D Davis <davisdf@gmail.com>
2023-02-21 18:07:05 -0500

    Updated solution for find the integral

--- FindTheIntegral.java
+++ FindTheIntegral.java
@@ -1,12 +1,69 @@
-public class FindTheIntegral {
+/* 8KYU CodeWars Find The Integral
+
+Create a function that finds the integral of the expression passed.
+
+In order to find the integral all you need to do is add one to the 
+exponent (the second argument), and divide the coefficient (the first argument) 
+by that new number.
+
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, 
+and divided the coefficient by that new number).
+
+Notes:
+
+The output should be a string.
+The coefficient and exponent is always a positive integer.
+
+Examples
+ 3, 2  -->  "1x^3"
+12, 5  -->  "2x^6"
+20, 1  -->  "10x^2"
+40, 3  -->  "10x^4"
+90, 2  -->  "30x^3"
+
+*/
+
+public class Kata {
 
     public static String integrate(int coefficient, int exponent) {
         // TODO implement the integration
-      
-      int result1 = (exponent + 1);
-      int result2 = (coefficient / result1);
-      
-      return (result2 + "x^"+ result1);
+      int x = exponent + 1;
+      int y = coefficient / x;
+      String z = String.valueOf(y);
+      String a = String.valueOf(x);
+      return z + "x^" + a;
     }
   
 }
+/*-------------------------------------------------------------------------
+public class Kata {
+  public static String integrate(int coefficient, int exponent) {
+    int newExponent = exponent + 1;
+    return String.format("%dx^%d", coefficient / newExponent, newExponent);
+  }
+}
+--------------------------------------------------------------------------
+public class Kata {
+
+    public static String integrate(int coefficient, int exponent) {
+        return coefficient / ++exponent + "x^" + exponent;
+    }
+  
+}
+---------------------------------------------------------------------------
+public class Kata {
+
+    public static String integrate(int coefficient, int exponent) {
+        return String.valueOf(coefficient/(exponent+1)+ "x^" + (exponent+1));
+    }
+}
+
+
+
+
+
+
+
+
+
+*/
