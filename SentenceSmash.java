9f6d8905250ad640746a053379bf71eb27272162
D Davis <davisdf@gmail.com>
2023-02-27 20:42:32 -0500

    Updated solution for Sentence Smash

--- SentenceSmash.java
+++ SentenceSmash.java
@@ -1,11 +1,11 @@
-/*  8KYU Sentence Smash
+/* 8KYU CodeWars Sentence Smash
 
-Write a function that takes an array of words and smashes 
-them together into a sentence and returns the sentence. 
-You can ignore any need to sanitize words or add punctuation, 
-but you should add spaces between each word. Be careful, 
-there shouldn't be a space at the beginning or the end of 
-the sentence!
+Sentence Smash
+Write a function that takes an array of words and smashes them together 
+into a sentence and returns the sentence. You can ignore any need to 
+sanitize words or add punctuation, but you should add spaces between 
+each word. Be careful, there shouldn't be a space at the beginning or 
+the end of the sentence!
 
 Example
 
@@ -13,11 +13,90 @@ Example
 
 */
 
+
 import java.util.Arrays;
 
 public class SmashWords {
 
 	public static String smash(String... words) {
-        return String.join(" ", words);
+    // TODO Write your code below this comment please
+    
+    String result = "";
+    for (String x : words) {
+      if (words[0] == "") {
+        return result;
+      }
+      else {
+        result += x + " ";
+      }
+      
+    }
+    int x = result.length() - 1;
+    return result.isEmpty() ? "" : result.substring(0, x);
   }
 }
+/*-------------------------------------------------------------------------------------------
+
+public class SmashWords {
+	public static String smash(String... words) {
+    return String.join(" ", words);
+  }
+}
+---------------------------------------------------------------------------------------------
+import java.util.Arrays;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    return words != null ? String.join(" ", words) : null;
+  }
+}
+--------------------------------------------------------------------------------------------
+import java.util.Arrays;
+import java.lang.StringBuilder;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    StringBuilder sb = new StringBuilder();
+    for (String word : words)
+    {
+      sb.append(word + " ");
+    }
+    return sb.toString().trim();
+  }
+}
+---------------------------------------------------------------------------------------
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    return Stream.of(words).collect(Collectors.joining(" "));
+  }
+}
+----------------------------------------------------------------------------------------
+import java.util.Arrays;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+		if (words != null)
+			return String.join(" ", Arrays.asList(words));
+		else
+			return null;
+	}
+}
+----------------------------------------------------------------------------------------
+import java.util.Arrays;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    return Arrays.asList(words).toString().replaceAll("\\[|\\]|,", "");
+  }
+}
+
+
+*/
