1c4d9c27cfe71b9e1944bfc8c85e48eb51b5ed9f
D Davis <davisdf@gmail.com>
2023-03-03 18:45:40 -0500

    Updated solution for Bumps In The Road

--- BumpsInTheRoad.java
+++ BumpsInTheRoad.java
@@ -14,3 +14,42 @@ public class BumpsTheRoad {
     return road.chars().filter(i->(char)i=='n').count() <= 15 ? "Woohoo!" : "Car Dead";
   }
 }
+/*------------------------------------------------------------------------------------------
+public class BumpsTheRoad {
+  public static String bumps(final String road) {
+    int count=0;
+    for (int i = 0; i < road.length(); i++) {
+      if (road.charAt(i) =='n')
+        count++;
+    }  
+    if (count <= 15)
+      return "Woohoo!";
+    else
+      return "Car Dead";
+  }
+}
+--------------------------------------------------------------------------------------------
+public class BumpsTheRoad {
+  private static final Integer SAFE_COUNT = 15;
+  private static final String FLAT = "_";
+  private static final String SUCCESS_MESSAGE = "Woohoo!";
+  private static final String FAILURE_MESSAGE = "Car Dead";
+  
+  public static String bumps(final String road) {
+    String bumps = road.replaceAll(FLAT, "");
+    boolean safe = bumps.length() <= SAFE_COUNT; 
+    return safe ? SUCCESS_MESSAGE : FAILURE_MESSAGE;
+  }
+}
+-------------------------------------------------------------------------------------------
+ class BumpsTheRoad {
+ static String bumps(String road) {
+    return road.replace("_", "").length() > 15 ? "Car Dead" : "Woohoo!";
+  }
+}
+
+
+
+
+
+*/
