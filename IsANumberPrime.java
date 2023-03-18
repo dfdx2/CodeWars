81a37410516e85ac07f89ad0a7c2ebf9fefa2d5e
D Davis <davisdf@gmail.com>
2023-03-18 18:50:12 -0400

    Updated solution for Is A Number Prime

--- IsANumberPrime.java
+++ IsANumberPrime.java
@@ -23,18 +23,95 @@ is_prime(-1)   false
 */
 
 public class Prime {
-    public static boolean isPrime(int num) {
-      if (num <= 1) {
-        return false;
-      }
-      else {
-        int newSquare = (int)Math.sqrt(num) + 1;
-        for (int i = 2; i < newSquare; i++) {
-          if (num % i == 0) {
-            return false;
-          }
+  public static boolean isPrime(int num) {
+    if (num <= 1) {
+      return false;
+    }
+    else {
+      int newnum = (int)Math.sqrt(num) + 1;
+      for (int i = 2; i < newnum; i++) {
+        if (num % i == 0) {
+          return false;
         }
       }
-      return true;
-      }
+    }
+    return true;
   }
+}
+/*---------------------------------------------------------------------------
+public class Prime {
+  public static boolean isPrime(int num) {
+    return num > 1 && java.math.BigInteger.valueOf(num).isProbablePrime(20);
+  }
+}
+------------------------------------------------------------------------------
+public class Prime {
+
+  public static boolean isPrime(int num) {
+    // Filter out negative numbers, 0 and 1
+    if (num <= 1)
+      return false;
+    // Factors of a number form pairs and the middle pair is the square root so no
+    // reason to test numbers beyond that
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+-------------------------------------------------------------------------
+public class Prime {
+  public static boolean isPrime(int num) {
+    if (num == 2) {
+        return true;
+    }
+    
+    if (num < 2 || num % 2 == 0) {
+        return false;
+    }
+
+    for (int i = 3; num >= i*i; i+=2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+  }
+}
+--------------------------------------------------------------------------
+import java.math.BigInteger;
+public class Prime {
+    public static boolean isPrime(int num) {
+    
+        if (num < 2) { return false; }
+    
+        BigInteger result = BigInteger.valueOf(num);
+        return result.isProbablePrime((int)Math.log(num));
+    }
+}
+----------------------------------------------------------------------------
+public class Prime {
+  public static boolean isPrime(int num) {
+    return num >= 0 && java.math.BigInteger.valueOf(num).isProbablePrime(42);
+  }
+}
+----------------------------------------------------------------------------
+import static java.math.BigInteger.valueOf;
+
+class Prime {
+  static boolean isPrime(int num) {
+    return num > 1 && valueOf(num).isProbablePrime(10);
+  }
+}
+---------------------------------------------------------------------------
+import org.apache.commons.math3.primes.Primes;
+public class Prime {
+  public static boolean isPrime(int num) {
+    return Primes.isPrime(num);
+  }
+}
+
+
+*/
