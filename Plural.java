240d56ca6c522d290b01cc291d860f5b1edd45aa
D Davis <davisdf@gmail.com>
2023-03-31 19:05:54 -0400

    Updated solution for Plural

--- Plural.java
+++ Plural.java
@@ -1,3 +1,23 @@
+/* 8KYU CodeWars Plural
+
+We need a simple function that determines if a plural is needed or not. 
+It should take a number, and return true if a plural should be used with 
+that number or false if not. This would be useful when printing out a 
+string such as 5 minutes, 14 apples, or 1 sun.
+
+You only need to worry about english grammar rules for this kata, where 
+anything that isn't singular (one of something), it is plural (not one 
+of something).
+
+All values will be positive integers or floats, or zero.
+
+*/
+public class Plural{
+  public static boolean isPlural(float f){
+    return f != 1;
+  }
+}
+/*-----------------------------------------------------------------------
 public class Plural{
   public static boolean isPlural(float f){
     if (f != 1) {
@@ -8,3 +28,28 @@ public class Plural{
     }
   }
 }
+---------------------------------------------------------------------------
+public class Plural{
+  public static boolean isPlural(float f){
+    return f != 1f;
+  }
+}
+---------------------------------------------------------------------------
+public class Plural{
+  public static boolean isPlural(float f){
+    return (f == 1f) ? false : true;
+  }
+}
+----------------------------------------------------------------------------
+public class Plural{
+  public static boolean isPlural(float f){
+    System.out.println(f);
+    if( f>1 || f<1 || Float.compare(f, Float.NaN) ==0 || 
+    	Float.compare(f, Float.NEGATIVE_INFINITY) ==0   ||
+      Float.compare(f, Float.POSITIVE_INFINITY) ==0 )
+    	return true;
+    return false;
+  }
+}
+
+*/
