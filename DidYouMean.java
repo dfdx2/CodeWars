5aae13ec6597206468775d7368e89b8ef322fa5f
D Davis <davisdf@gmail.com>
2023-04-01 17:35:06 -0400

    Updated solution for Did You Mean

--- DidYouMean.java
+++ DidYouMean.java
@@ -1,12 +1,14 @@
-/*    5KYU  DID YOU MEAN   
-I'm sure, you know Google's "Did you mean ...?", when you entered a search term and 
-mistyped a word. In this kata we want to implement something similar.
+/*    5KYU CodeWars DID YOU MEAN   
+
+I'm sure, you know Google's "Did you mean ...?", when you entered a search 
+term and mistyped a word. In this kata we want to implement something similar.
 You'll get an entered term (lowercase string) and an array of known words 
 (also lowercase strings). Your task is to find out, which word from the dictionary 
 is most similar to the entered one. The similarity is described by the minimum number 
 of letters you have to add, remove or replace in order to get from the entered word 
 to one of the dictionary. The lower the number of required changes, the higher the 
 similarity between each two words.
+
 Same words are obviously the most similar ones. A word that needs one letter to be 
 changed is more similar to another word that needs 2 (or more) letters to be changed. 
 E.g. the mistyped term berr is more similar to beer (1 letter to be replaced) 
