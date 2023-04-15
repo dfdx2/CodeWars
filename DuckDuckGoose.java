c44634ae3a494bda2821cfe60021360beb67e148
D Davis <davisdf@gmail.com>
2023-04-15 15:55:31 -0400

    Added solution for Duck Duck Goose

---
+++ DuckDuckGoose.java
@@ -0,0 +1,64 @@
+/* 8KYU CodeWars Duck Duck Goose
+
+The objective of Duck, duck, goose is to walk in a circle, tapping on 
+each player's head until one is chosen.
+
+Task: Given an array of Player objects (an array of associative arrays 
+in PHP) and an index (1-based), return the name of the chosen 
+Player(name is a property of Player objects, e.g Player.name)
+
+Example:
+
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+
+// PHP only
+duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"]
+
+*/
+public class Kata {
+  public static String duckDuckGoose(Player[] players, int goose) {
+    // Note: Player objects have a String field called 'name'.
+     int result = goose % players.length;
+    return (result == 0) ? players[players.length - 1].name : players[result - 1].name;
+  }
+}
+/*---------------------------------------------------------------------------
+public class Kata {
+  public static String duckDuckGoose(Player[] players, int goose) {
+    return players[(goose - 1) % players.length].name;
+  }
+}
+------------------------------------------------------------------------------
+public class Kata {
+  public static String duckDuckGoose(Player[] players, int goose) {
+    // Note: Player objects have a String field called 'name'.
+    
+    int a=players.length;
+    int b=goose%a-1;
+    if(b<0){
+        b=b+a;
+    }
+     return players[b].name;
+  }
+}
+----------------------------------------------------------------------------------
+class Kata {
+    static String duckDuckGoose(final Player[] players, final int goose) {
+        final int index = (goose - 1) % players.length;
+        return players[index].name;
+    }
+}
+---------------------------------------------------------------------------------
+class Kata {
+  static String duckDuckGoose(Player[] players, int goose) {
+    return players[--goose % players.length].name;
+  }
+}
+---------------------------------------------------------------------------------
+
+
+*/
