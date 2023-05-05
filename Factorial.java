f9048921a565ec0ee3b701cc4972587384b096e5
D Davis <davisdf@gmail.com>
2023-05-05 19:33:53 -0400

    Added solution for Factorial

--- Factorial.java
+++ Factorial.java
@@ -1,60 +1,55 @@
-/*  7KYU CodeWars  Factorial
+/* 7KYU CodeWars Factorial
 
-In mathematics, the factorial of a non-negative integer n, 
-denoted by n!, is the product of all positive integers less 
-than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. 
-By convention the value of 0! is 1.
+Your task is to write function factorial.
 
-Write a function to calculate factorial for a given input. 
-If input is below 0 or above 12 throw an exception of type 
-ArgumentOutOfRangeException (C#) or IllegalArgumentException 
-(Java) or RangeException (PHP) or throw a RangeError 
-(JavaScript) or ValueError (Python) or return -1 (C).
-
-More details about factorial can be found here.
+https://en.wikipedia.org/wiki/Factorial
 
 */
 
 public class Factorial {
 
-  public int factorial(int n) {
-    // Happy coding :-)
-    int result = n;
-    if (n < 0 || n > 12) {
-      throw new IllegalArgumentException();
+    public static long factorial(int n) {
+        return (n == 0) ? 1 : n * factorial(n - 1);
     }
-    
-    if (n == 0) {
-      result = 1;
-    }
-    for (int i = n - 1; i > 1; --i) {
-      result = result * i;
-    }
-    return result;
-  }
 }
-
-/*--------------------------------------------------------------------------
-
+/*----------------------------------------------------------------------------
 public class Factorial {
-  public int factorial(int n) {
-    if(n < 0 || n > 12) throw new IllegalArgumentException("Useless fuffery!");
-    return n <= 1 ? 1 : n * factorial(n - 1);
-  }
+
+    public static long factorial(int n) {
+        
+        if (n == 0) {
+           return 1;
+        }
+        else {
+           return n * factorial(n - 1);
+        }
+    }
 }
 ------------------------------------------------------------------------------
+import java.util.stream.LongStream;
 
 public class Factorial {
 
-  public int factorial(int n) {
-        if (n == 0)
-            return 1;
-
-        if (n < 0 || n > 12)
-            throw new IllegalArgumentException();
-
-        return n * factorial(n - 1);
+    public static long factorial(int n) {
+        return LongStream.rangeClosed(2L, n).parallel().reduce(1L, (a, b) -> a * b);
     }
+}
+-----------------------------------------------------------------------------
+import java.util.stream.LongStream;
+
+public class Factorial {
+
+    public static long factorial(int n) {
+        return n < 2 ? 1 : LongStream.rangeClosed(1, n).reduce((x,y) -> x * y).orElse(0);
+    }
+}
+------------------------------------------------------------------------------
+public class Factorial {
+
+    public static long factorial(int n) {
+        if (n==0) return 1;
+        if (n>1) return n * factorial(n-1);
+        return n;    }
 }
 
 
