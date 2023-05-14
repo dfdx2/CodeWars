ceca75becb9b9ebdbfdcbaa362623a9b27eb40a4
D Davis <davisdf@gmail.com>
2023-05-14 13:36:27 -0400

    Added solution for Traffic Lights

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,12 +1,8 @@
-/*  Thinkful - Logic Drills: Traffice Light 
+/* 8KYU CodeWars Logic Drills : Traffic Light
 
-You're writing code to control your town's traffic lights. 
-You need a function to handle each change from green, to 
-yellow, to red, and then to green again.
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
-Complete the function that takes a string as an argument 
-representing the current state of the light and returns 
-a string representing the state the light should change to.
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 For example, when the input is green, output should be yellow.
 
@@ -15,22 +11,11 @@ For example, when the input is green, output should be yellow.
 public class TrafficLights {
 
   public static String updateLight(String current) {
-    if (current == "green") {
-      return ("yellow");
-    }
-    else if (current == "yellow") {
-      return ("red");
-    }
-    else {
-      return ("green");
-    }
-  
-  
+    return (current == "green") ? "yellow" : (current == "yellow") ? "red" : (current == "red") ? "green" : null;
   }
-  
 }
-/*---------------------------------------------------------------
- public class TrafficLights {
+/*----------------------------------------------------------------------------
+public class TrafficLights {
 
   public static String updateLight(String current) {
     switch (current) {
@@ -40,8 +25,9 @@ public class TrafficLights {
       default: throw new IllegalArgumentException();
     }
   }
+
 }
--------------------------------------------------------------------
+-------------------------------------------------------------------------------
 public class TrafficLights {
 
   public static String updateLight(String current) {
@@ -51,7 +37,7 @@ public class TrafficLights {
   }
   
 }
--------------------------------------------------------------------
+------------------------------------------------------------------------------
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 
@@ -68,7 +54,7 @@ public class TrafficLights {
   }
   
 }
---------------------------------------------------------------------
+------------------------------------------------------------------------------
 public class TrafficLights {
 
   public static String updateLight(String current) {
@@ -79,29 +65,31 @@ public class TrafficLights {
   }
   
 }
---------------------------------------------------------------------
-public class TrafficLights {
+-------------------------------------------------------------------------------
+import java.util.List;
+import java.util.Arrays;
 
+public class TrafficLights {
+  private static List<String> lights = Arrays.asList("green", "yellow", "red");
+  
   public static String updateLight(String current) {
-    if(current == "green") return "yellow";
-    if(current == "yellow") return "red";
-    else return "green";
+    //return the next color
+    return lights.get((lights.indexOf(current)+1)%lights.size());
   }
   
 }
-----------------------------------------------------------------
+-----------------------------------------------------------------------------
+import java.util.HashMap;
+
 public class TrafficLights {
 
   public static String updateLight(String current) {
-    return switch (current) {
-      case "red" -> "green";
-      case "yellow" -> "red";
-      case "green" -> "yellow";
-      default -> throw new IllegalArgumentException();
-    };
+    HashMap<String, String> lights = new HashMap<>();
+    lights.put("green", "yellow"); lights.put("yellow", "red"); lights.put("red", "green");
+    return lights.get(current);
   }
-
+  
 }
------------------------------------------------------------------
+
 
 */
