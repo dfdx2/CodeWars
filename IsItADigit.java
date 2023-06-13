df246895987482d40d60f74c73deeccb71312b27
D Davis <davisdf@gmail.com>
2023-06-13 17:55:15 -0400

    Added solution for Is It A Digit

---
+++ IsItADigit.java
@@ -0,0 +1,13 @@
+/* 8KYU CodeWars REGEXP Basics - Is It A Digit
+
+Implement String#digit? (in Java StringUtils.isDigit(String)), 
+which should return true if given object is a digit (0-9), false otherwise.
+
+*/
+
+public class StringUtils {
+  
+  public static boolean isDigit(String s) {
+    return s.matches("[0-9]");
+  }
+}
