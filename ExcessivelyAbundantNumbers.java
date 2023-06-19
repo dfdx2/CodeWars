470378a4cd312f79e9e0e4cbe53114cc66363363
D Davis <davisdf@gmail.com>
2023-06-19 11:00:38 -0400

    Added solution for Excessively Abundant Numbers

--- ExcessivelyAbundantNumbers.java
+++ ExcessivelyAbundantNumbers.java
@@ -1,30 +1,34 @@
-/*    7KYU CodeWars  Excessively Abundant Numbers  
-An abundant number or excessive number is a number for which the sum 
-of its proper divisors is greater than the number itself.
-The integer 12 is the first abundant number. Its proper divisors 
-are 1, 2, 3, 4 and 6 for a total of 16 (> 12).
+/* 7KYU CodeWars Excessively Abundant Numbers
+
+An abundant number or excessive number is a number for which the sum of its 
+roper divisors is greater than the number itself.
+
+The integer 12 is the first abundant number. Its proper divisors are 1, 2, 3, 
+4 and 6 for a total of 16 (> 12).
+
 Derive function abundantNumber(num)/abundant_number(num) which returns 
 true/True/.true. if num is abundant, false/False/.false. if not.
 
 */
 
-import java.util.*;
-import java.util.stream.*;
-
 public class Kata {
     
-    public static boolean abundantNumber(int num) {
-        List<Integer> divided = new ArrayList<>();
-        IntStream.range(1, num).forEach(n -> {
-          if (num % n == 0) {
-            divided.add(n);
-          }
-        });
-      return num < divided.stream().mapToInt(i->i).sum();
+  public static boolean abundantNumber(int num) {
+    int count = 0;
+    int i = 1;
+    while (i < num) {
+      if (num % i == 0) {
+        count += i;
+        i++;
+      }
+      else {
+        i++;
+      }
     }
-    
+    return count > num;
+  }
 }
-/*----------------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
 import java.util.stream.IntStream;
 
 public class Kata {
@@ -34,23 +38,62 @@ public class Kata {
     }
     
 }
--------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+import java.util.stream.IntStream;
+
+public class Kata {
+  
+  public static boolean abundantNumber(int n) {
+    return IntStream.rangeClosed(1, n / 2).reduce(0, (s, m) -> s + (n % m == 0 ? m : 0)) > n;
+  }
+}
+----------------------------------------------------------------------------
+import java.util.ArrayList;
+
 public class Kata {
     
     public static boolean abundantNumber(int num) {
-      int sum=0;
-      for(int i=1;i<num;i++){
-        if(num%i==0){
-          sum=sum+i;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) list.add(i);
         }
-      }
-    return (sum>num)?true:false;
+        return list.stream().mapToInt(a -> a).sum() > num;
     }
- }
--------------------------------------------------------------------------------------
+    
+}
+-------------------------------------------------------------------------------
+import java.util.stream.IntStream;
+  
+public class Kata {
+    public static boolean abundantNumber(int num) {
+        return num < IntStream.range(1,num).filter(x -> num%x==0).sum();
+    }
+}
+------------------------------------------------------------------------------
+import java.util.stream.IntStream;
 
+public class Kata {
+  public static boolean abundantNumber(int num) {
+    return num < IntStream.rangeClosed(1, num/2)
+        .filter(n -> num % n == 0)
+        .sum();
+  }
+}
+--------------------------------------------------------------------------
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-
+public class Kata {
+    
+    public static boolean abundantNumber(int num) {
+        int[] properDivisors = IntStream.range(1, num)
+				        		.filter(a -> num % a== 0)
+				        		.toArray();
+        int sum = Arrays.stream(properDivisors).sum();
+        return sum >= num;
+    }
+    
+}
 
 
 
