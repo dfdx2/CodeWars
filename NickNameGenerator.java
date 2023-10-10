71232c6eaafd54c5e1a919d6b2e5dac37eecd1a6
D Davis <davisdf@gmail.com>
2023-10-10 17:44:48 -0400

    Updated Nickname Generator solution

--- NickNameGenerator.java
+++ NickNameGenerator.java
@@ -1,7 +1,9 @@
-/*   Nickname Generator
+/* 7KYU Nickname Generator
 
-Write a function, nicknameGenerator that takes a string name as an argument
-and returns the first 3 or 4 letters as a nickname.
+Nickname Generator
+
+Write a function, nicknameGenerator that takes a string name as an 
+argument and returns the first 3 or 4 letters as a nickname.
 
 If the 3rd letter is a consonant, return the first 3 letters.
 
@@ -19,28 +21,29 @@ Notes:
 
 Vowels are "aeiou", so discount the letter "y".
 Input will always be a string.
-Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+Input will always have the first letter capitalised and the rest 
+lowercase (e.g. Sam).
 The input can be modified
+
 */
-
-
-class NickNameGenerator {
+class Generator {
       public static String nickname (String name) {
-      
-
         if (name.length() < 4) {
-          return ("Error: Name too short");
+          return "Error: Name too short";
         }
-        else if(name.charAt(2) == 'a'|| name.charAt(2) == 'e'|| name.charAt(2) == 'i' || name.charAt(2) == 'o' || name.charAt(2) == 'u') {
-          return name.substring(0,4);
+        String x = name.substring(0,3);
+        String y = name.substring(0,4);
+        char ch = name.charAt(2);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+          return y;
         }
         else {
-          return name.substring(0,3);
+          return x;
         }
       }
 }
-/*______________________________________________________________________________________
- * class Generator {
+/*--------------------------------------------------------------------------
+class Generator {
     private static final String VOWELS = "aeiou";
 
     static String nickname(final String name) {
@@ -51,20 +54,92 @@ class NickNameGenerator {
             name.substring(0, 4) : name.substring(0, 3);
     }
 }
- *_______________________________________________________________________________________ 
- * 
- * class Generator {
+----------------------------------------------------------------------------
+class Generator 
+{
+      public static String nickname (String name) 
+      {
+          char word_char[] = name.toCharArray();
+          String result = "";
+        
+          if(word_char.length >= 4)
+          {
+            switch(word_char[2])
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    result = name.substring(0, 4);
+                break;
+                default:
+                    result = name.substring(0, 3);
+                break;
+            }
+          }
+          else
+          {
+              result = "Error: Name too short";
+          }
+        
+          return result;
+      }
+}
+---------------------------------------------------------------------------
+class Generator {
     public static String nickname (String name) {
       if(name.length() < 4) return "Error: Name too short";
       return name.matches("^.{2}[euoai].+$") ? name.substring(0, 4) : name.substring(0, 3);
     }
 }
- * ___________________________________________________________________________________________
- * 
- * class Generator {
+-----------------------------------------------------------------------------
+class Generator {
   public static String nickname (String s) {
     return s.length() < 4 ? "Error: Name too short" : s.substring(0,  (""+s.charAt(2)).matches("[aeiou]") ? 4 : 3);
   }
 }
- *  
- */
+-------------------------------------------------------------------------------
+class Generator {
+      public static String nickname (String name) {
+      // your code goes here
+        if(name.length() < 4){
+          return "Error: Name too short";
+        }else if(name.charAt(2)=='a'||name.charAt(2)=='e'||name.charAt(2)=='i'||name.charAt(2)=='o'||name.charAt(2)=='u'){
+          return name.substring(0,4);
+        }else{
+          return name.substring(0,3);
+        }
+      }
+}
+------------------------------------------------------------------------------
+import java.util.Optional;
+
+class Generator {
+  public static String nickname (String name) {
+    return Optional.ofNullable(name)
+                   .filter(n -> n.length() >= 4)
+                   .map(n -> "aeiou".contains(n.substring(2, 3)) ? n.substring(0, 4) : n.substring(0, 3))
+                   .orElse("Error: Name too short");
+  }
+}
+------------------------------------------------------------------------------
+class Generator {
+      public static String nickname (String name) {
+      
+      
+      if(name.length() < 4){
+            return "Error: Name too short";
+        }
+
+        return String.valueOf(name.charAt(2))
+                .matches("[aeiou]") ?
+                name.substring(0,4) :
+                name.substring(0,3);
+      
+      }
+}
+
+
+
+*/
