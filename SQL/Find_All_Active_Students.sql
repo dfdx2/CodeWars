76fc927354ef4a2627d495c33a78dce37c96098e
D Davis <davisdf@gmail.com>
2024-03-11 07:04:37 -0400

    Added sql solution for Find All Active Students

---
+++ SQL/Find_All_Active_Students.sql
@@ -0,0 +1,21 @@
+/* 8KYU CodeWars Find All Active Students
+
+Create a simple SELECT query to display student information of all ACTIVE students.
+
+TABLE STRUCTURE:
+
+students
+Id (integer)	FirstName (text)	LastName (text)	IsActive (boolean)
+
+Note:
+
+IsActive (true = 1 or false = 0)
+see specification: Datatypes In SQLite
+
+
+
+
+*/
+SELECT *
+FROM students
+WHERE IsActive = 1;
