065705d76cbab3af27fc02769f30d73fa6662a8d
D Davis <dfdx2.0@gmail.com>
2024-05-11 19:23:47 -0400

    Added solution for javascript problem Making Copies

---
+++ JS/Making_Copies.js
@@ -0,0 +1,57 @@
+/* 7KYU CodeWars Making Copies
+
+Write a function that takes a list (in Python) or 
+array (in other languages) of numbers, and makes 
+a copy of it.
+
+Note that you may have troubles if you do not return 
+an actual copy, item by item, just a pointer or an 
+alias for an existing list or array.
+
+If not a list or array is given as a parameter in 
+interpreted languages, the function should raise 
+an error.
+
+Examples:
+
+t = [1, 2, 3, 4]
+tCopy = copyList(t)
+t[1] += 5
+t = [1, 7, 3, 4]
+tCopy = [1, 2, 3, 4]
+
+
+
+*/
+
+function copyList(l){
+  let lcopy = [...l];
+  return lcopy;
+}
+/*-----------------------------------------------------------------
+function copyList(l) {
+  return l.slice();
+}
+-------------------------------------------------------------------
+function copyList(l){
+  return [...l];
+}
+-------------------------------------------------------------------
+function copyList(l){
+  return (Array.isArray(l)) ? l.slice(0) : "error";
+}
+-------------------------------------------------------------------
+function copyList(l){return [].concat(l);}
+-------------------------------------------------------------------
+const copyList = (l) => l.map(e => e);
+-------------------------------------------------------------------
+function copyList(l){
+  return JSON.parse(JSON.stringify(l));
+}
+-------------------------------------------------------------------
+function copyList(list){
+  return list.map(function(obj){return obj});
+}
+-------------------------------------------------------------------
+var copyList = (l) => l.slice()
+*/
