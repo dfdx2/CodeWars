efa039815788cbf8cdbdbb71bb9d3c6d28c755b1
D Davis <dfdx2.0@gmail.com>
2024-07-22 19:44:55 -0400

    Added js solution for returning strings

---
+++ JS/Returning_Stringsjs.js
@@ -0,0 +1,76 @@
+/* 8KYU CodeWars Returning Strings
+
+Make a function that will return a greeting statement 
+that uses an input; your program should return, "Hello, 
+<name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program 
+may not execute properly]
+
+*/
+function greet(name){
+  return "Hello, " + name + " how are you doing today?";
+}
+/*-------------------------------------------------------
+const greet = name => `Hello, ${name} how are you doing today?`;
+---------------------------------------------------------
+function greet(name) {
+  return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`
+}
+---------------------------------------------------------
+function greet(name) {
+ if ((!(NaN !== NaN)) ? false : true / (Math.sin(90 * Math.PI / 180))) {
+   const 山 = '010101h01H1010h1010'
+   const 人 = '0e1r0010100e010101001010'
+   const 口 = '0l101l0101111000001010100010010'
+   const 刀 = 'dl001l1011010101011010111100010101'
+   const 目 = '1d000y10100101101011111100001011110010110101111000000010010101'
+   const 日 = '01101o11110001001010100010110101111000011010111100010010000100010'
+   const 女 = 'o00a1d10101000101001011010111100010010101010100010100100010010101010100101'
+   const 鳥 = '0t010o01010111100001010010110101111000100101010100010111110001010101001010010'
+   const 子 = 'y0101g00101111111101111000100101010010111100010011101010100010100101101011110001010'
+   const 木 = '10101o001011111111010111100010010101010001010010110111101111000101010111100000101000101001011'
+   const 水 = 'o010100101011101010111001000101001010111010101110010001010010101110101011101010111100010010101010100101001'
+   const 月 = 'wu01011101010111001010110101010101000101001010111010101110010101101010101010001010010101110101011100101011101'
+   const 沐 = '1010101010010101010101010101010101010101 doing 0101010101010101010101010100101010110010101010101010101011010101010 '
+   const thisIsSupposedToBeTheSpaceCharButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere = ' '
+   const thisIsSupposedToBeTheCommaCharButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere = ','
+   const thisIsSupposedToBeTheQuestionMarkSignButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere = '?'
+   
+   let _a = ''
+   let ____________ = ''
+   
+   const _=_a.concat(山.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))+7)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const ___ =_.concat(人.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))-1,Math.floor(Math.acos(1)+9+1-4/8-8+Math.cos(7))))
+   const _____=___.concat(口.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488)))[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]])
+   const _______=_____.concat(口.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488)))[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]])
+   const _________=_______.concat(木.slice(5)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const __________=_________.concat(thisIsSupposedToBeTheCommaCharButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere)
+   const ___________=__________.concat(thisIsSupposedToBeTheSpaceCharButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere)
+   const _____________ = ____________.concat(山.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))+4)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const ______________ = _____________.concat(日.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488)))[0])
+   const _______________ = ______________.concat(月.slice(Math.floor(Math.acos(1)+9+1-4/8-8+Math.cos(7))-1-1)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const ________________ = _______________.concat(thisIsSupposedToBeTheSpaceCharButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere)
+   const _________________ = ________________.concat(女.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488))-2)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const __________________= _________________.concat(人.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488))-2)[Math.trunc(000+0)])
+   const ___________________=__________________.concat(人.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))-1,Math.floor(Math.acos(1)+9+1-4/8-8+Math.cos(7))))
+   const ____________________=___________________.concat(thisIsSupposedToBeTheSpaceCharButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere)
+   const _____________________=____________________.concat(子.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1)[[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]]])
+   const ______________________=_____________________.concat(木.slice(5)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const _______________________=______________________.concat(月.slice(Math.floor(Math.acos(1)+9+1+1-4/8-8+Math.cos(7))-1-1)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const ________________________=_______________________.concat(沐.slice((Math.tanh(45) + (Math.min(-47, 150, 30, 20, 8, 200)) + 86),Math.trunc(3**2-2+1514-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E)))
+   const _________________________=________________________.concat(鳥.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))+0)[1+2+3-6])
+   const __________________________=_________________________.concat(水.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const ___________________________=__________________________.concat(目.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7)))[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const _____________________________=___________________________.concat(女.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488))-2)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+   const ______________________________=_____________________________.concat(子.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1)[[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]]])
+   const _______________________________=______________________________.concat(thisIsSupposedToBeTheQuestionMarkSignButSadlyJavaScriptDoesntAllowMeToNameMyVariableAsSuchSorryGuysGottaBreakThePatternHere)
+   
+   return `${__________} ${name} ${_______________________________}`}}
+---------------------------------------------------------
+function greet(name){
+  return "Hello, <name> how are you doing today?".replace("<name>", name);
+}
+--------------------------------------------------------
+const greet=n=>`Hello, ${n} how are you doing today?`;
+*/
