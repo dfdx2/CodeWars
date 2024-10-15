f530733b52375851dd07867e9feda422a9ce91d5
D Davis <dfdx2.0@gmail.com>
2024-10-15 17:45:47 -0400

    Added updated solution for Quadrants

--- Quadrants.java
+++ Quadrants.java
@@ -27,14 +27,19 @@ More on quadrants: Quadrant (plane geometry) - Wikipedia
 
 
 */
-
+public class Quadrants {
+  public static int quadrant(int x, int y) {
+    return x > 0 && y > 0 ? 1 : x < 0 && y < 0 ? 3 : x > 0 && y < 0 ? 4 : 2;
+  }
+}
+/*---------------------------------------------------------------------------
 public class Kata {
   
     public static int quadrant(int x, int y) {
       return (x > 0 && y > 0) ? 1 : (x < 0 && y > 0) ? 2 : (x < 0 && y < 0) ? 3 : 4;
     }
 }
-/*--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 public class Kata {
   
   public static int quadrant(int x, int y) {
@@ -67,4 +72,35 @@ public class Kata {
     return 3;
   }
 }
+-----------------------------------------------------------------------------
+public class Kata {
+  
+  public static int quadrant(int x, int y) {
+    
+    int result = 0;
+    int dumb = 0;
+    int stooped = 0;
+    int retard = 0;
+    int plsstop = result;
+    int mf = 0;
+    int thiss = 1;
+    int dont = 2;
+    int makes = 3;
+    int sense = 4;
+    int iam = x;
+    int youare = y;
+
+      if(iam > dumb && youare > stooped) {
+        plsstop = thiss;
+      } else if(iam < stooped && youare > retard) {
+        plsstop = dont;
+      } else if(iam < retard && youare < dumb) {
+        plsstop = makes;
+      } else if(iam > mf && youare < mf) {
+        plsstop = sense;
+      }
+      return plsstop;
+  }
+}
+
 */
