ff911f74c59c4e0accc9a6f51a71904376d5d7d4
D Davis <dfdx2.0@gmail.com>
2024-10-19 10:14:04 -0400

    Updated solution for Simple Multiplication

--- SimpleMultiplication.java
+++ SimpleMultiplication.java
@@ -4,7 +4,13 @@ This kata is about multiplying a given number by
 eight if it is an even number and by nine otherwise.
 
 */
+public class SimpleMultiplication {
+  public static int simpleMultiplication(int n) {
+    return n % 2 == 0 ? n * 8 : n * 9;
+  }
+}
 
+/*------------------------------------------------------
 public class SimpleMultiplication {
     public static int simpleMultiplication(int n) {
         //your code here
@@ -16,7 +22,7 @@ public class SimpleMultiplication {
       }
     }
 }
-/*--------------------------------------------------------
+--------------------------------------------------------
 public class Sid {
     public static int simpleMultiplication (int n) {
         return n % 2 == 0 ? n * 8 : n * 9;
