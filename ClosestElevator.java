f16057cf91fecc89e1ec29b97af86dfc9b718d2d
D Davis <dfdx2.0@gmail.com>
2024-12-23 16:46:31 -0500

    Added solution for Closest Elevator

---
+++ ClosestElevator.java
@@ -0,0 +1,29 @@
+/* 8KYU CodeWars Closest Elevator
+
+Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function accepting 3 arguments (in order):
+
+left - The current floor of the left elevator
+right - The current floor of the right elevator
+call - The floor that called an elevator
+It should return the name of the elevator closest to the called floor ("left"/"right").
+
+In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+
+You can assume that the inputs will always be valid integers between 0-2.
+
+Examples:
+
+left right call   result
+  0    1     0    "left"
+  0    1     1    "right"
+  0    1     2    "right"
+  0    0     0    "right"
+  0    2     1    "right"
+
+
+*/
+public class ClosestElevator {
+    public static String call(int left, int right, int call){
+      return right == call || right == left || Math.abs(right - call) <= Math.abs(left - call) ? "right" : "left";
+    }
+}
